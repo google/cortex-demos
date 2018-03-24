e1753ddaeb2c319607dfb8455d7b9e34da067b54
Maxim Sloyko <m.sloyko@gmail.com>
2018-03-24 11:16:10 -0700

    Working simple test of vector table relocation

--- apps/nvic-hwtest/main.c
+++ apps/nvic-hwtest/main.c
@@ -7,7 +7,14 @@ void simple_handler(void) {
 }
 
 int main(void) {
-    nvic_set_handler(IRQ_SVCALL, simple_handler);
+    nvic_init();
+    nvic_set_handler(IRQ_PENDSV, simple_handler);
+    nvic_enable_irqs();
 
+    nvic_set_handler(1, simple_handler);
+    nvic_enable_irq(1);
+
+    nvic_irqset(IRQ_PENDSV);
+    nvic_irqset(1);
     while (1);
 }
