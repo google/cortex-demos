f3421f025e90535d8affbfa92872f365bef24bd9
Maxim Sloyko <m.sloyko@gmail.com>
2018-04-22 08:39:22 -0700

    use different app by default when programming

--- scripts/program.sh
+++ scripts/program.sh
@@ -7,7 +7,7 @@ OPENOCD=`which openocd`
 CFG_PATH="${SCRIPTDIR}/../configs/openocd"
 BOARD="nrf52dk"
 CHIP="nrf52"
-APP_NAME="nvic-hwtest"
+APP_NAME="rtc-blinker"
 
 OPENOCD_PATH="${CFG_PATH}/${BOARD}"
 OPENOCD_CFG="${CFG_PATH}/${BOARD}/program.cfg"
