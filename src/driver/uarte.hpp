75d1cbf9cb9306347a75810bb99151a036fe0e24
Maxim Sloyko <m.sloyko@gmail.com>
2018-05-12 16:36:28 -0700

    UART driver definition skeleton

--- src/driver/uarte.hpp
+++ src/driver/uarte.hpp
@@ -1,8 +1,29 @@
 #pragma once
 
-#include "driver/peripheral.hpp"
-
 #include <cstdint>
 
+#include "driver/peripheral.hpp"
+
 namespace driver {
+
+class UART : public Peripheral {
+    public:
+        enum class ID {
+            UARTE0,
+            UARTE1,
+            UARTE2,
+            UARTE3,
+
+            UART0,
+            UART1,
+            UART2,
+            UART3,
+            UART4,
+        };
+
+        UART(uint32_t base, unsigned int irq_n) : Peripheral(base, irq_n) {}
+
+        static UART* request_by_id(ID id);
+};
+
 }  // namespace driver
