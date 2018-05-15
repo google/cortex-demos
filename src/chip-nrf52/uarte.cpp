1f13bc5eaf5daf93ce29ea64335dddcbfd40a579
Maxim Sloyko <m.sloyko@gmail.com>
2018-05-14 19:57:57 -0700

    uarte: configure pins when requesting the driver

--- src/chip-nrf52/uarte.cpp
+++ src/chip-nrf52/uarte.cpp
@@ -1,12 +1,21 @@
 #include "driver/uarte.hpp"
 
 #include "memio.h"
+#include "pinctrl.hpp"
+
 #include "nrf52/periph_utils.hpp"
+#include "nrf52/pinctrl.hpp"
 
 
 namespace driver {
 
 namespace {
+
+// Offset for various registers
+
+
+constexpr auto kUarte0ID = 2;
+constexpr auto kUarte1ID = 40;
 
 class UARTE : public UART {
     public:
@@ -19,6 +28,22 @@ class UARTE : public UART {
 
             raw_write32(base_ + kEnableOffset, kEnableValue);
 
+            using pf = pinctrl::function;
+            auto func_group = base_ == periph::id_to_base(kUarte0ID) ? pf::UARTE0_GROUP : pf::UARTE1_GROUP;
+
+            for (int i = pf::UARTE_RTS; i <= pf::UARTE_RXD; ++i) {
+                auto pin_sel = pinctrl::request_function(func_group + i);
+                if (pin_sel > 0) {
+                    raw_write32(base_ + kPselZero + 4 * i, (pin_sel & kPselMask));
+                }
+            }
+
+            raw_writeptr(base_ + kRxdPtr, rx_buffer_);
+            raw_write32(base_ + kRxdMaxCnt, sizeof(rx_buffer_));
+
+            raw_writeptr(base_ + kTxdPtr, tx_buffer_);
+            raw_write32(base_ + kTxdMaxCnt, sizeof(tx_buffer_));
+
             return 0;
         }
 
@@ -26,12 +51,33 @@ class UARTE : public UART {
         static constexpr auto kEnableOffset = 0x500;
         static constexpr auto kEnableValue = 8;
 
+        static constexpr auto kPselRts = 0x508;
+        static constexpr auto kPselTxd = 0x50c;
+        static constexpr auto kPselCts = 0x510;
+        static constexpr auto kPselRxd = 0x514;
+        static constexpr auto kPselMask = 0x3f;
+        static constexpr auto kPselZero = kPselRts - 4;
+
+        static constexpr auto kRxdPtr = 0x534;
+        static constexpr auto kRxdMaxCnt = 0x538;
+        static constexpr auto kRxdAmount = 0x53c;
+
+        static constexpr auto kTxdPtr = 0x544;
+        static constexpr auto kTxdMaxCnt = 0x548;
+        static constexpr auto kTxdAmount = 0x54c;
+
+        static constexpr auto kTxBufferSize = 32;
+        static constexpr auto kRxBufferSize = 16;
+
         bool configured_ = false;
+
+        uint8_t tx_buffer_[kTxBufferSize];
+        uint8_t rx_buffer_[kRxBufferSize];
 
 };
 
-UARTE uarte0{2};
-UARTE uarte1{40};
+UARTE uarte0{kUarte0ID};
+UARTE uarte1{kUarte1ID};
 
 }  // namespace
 
--- tests/nrf52_uarte_test.cpp
+++ tests/nrf52_uarte_test.cpp
@@ -1,10 +1,32 @@
 #include "catch.hpp"
 
-#include "driver/uarte.hpp"
+#include <cstring>
+
 #include "mock_memio.hpp"
+
+#include "driver/uarte.hpp"
+#include "pinctrl.hpp"
+#include "nrf52/pinctrl.hpp"
 
 constexpr uint32_t uarte0_base = 0x40002000;
 constexpr uint32_t uarte1_base = 0x40028000;
+
+// Offsets for some registers used in tests
+constexpr auto psel_rts = 0x508;
+constexpr auto psel_txd = 0x50c;
+constexpr auto psel_cts = 0x510;
+constexpr auto psel_rxd = 0x514;
+
+constexpr auto rxd_ptr = 0x534;
+constexpr auto rxd_maxcnt = 0x538;
+constexpr auto rxd_amount = 0x53c;
+
+constexpr auto txd_ptr = 0x544;
+constexpr auto txd_maxcnt = 0x548;
+constexpr auto txd_amount = 0x54c;
+
+constexpr auto config = 0x56c;
+
 
 TEST_CASE("TEST UARTE API") {
     auto& mem = mock::get_global_memory();
@@ -16,6 +38,36 @@ TEST_CASE("TEST UARTE API") {
 
         // Check that the peripheral was enabled
         CHECK(mem.get_value_at(uarte0_base + 0x500) == 8);
+
+        // Check that the pins were configured
+        auto cts = pinctrl::get_pin(pinctrl::function::UARTE0_CTS);
+        CHECK(cts >= 0);
+        CHECK((mem.get_value_at(uarte0_base + psel_cts) & 0x1f) == cts);
+
+        auto rts = pinctrl::get_pin(pinctrl::function::UARTE0_RTS);
+        CHECK(rts >= 0);
+        CHECK((mem.get_value_at(uarte0_base + psel_rts) & 0x1f) == rts);
+
+        auto txd = pinctrl::get_pin(pinctrl::function::UARTE0_TXD);
+        CHECK(txd >= 0);
+        CHECK((mem.get_value_at(uarte0_base + psel_txd) & 0x1f) == txd);
+
+        auto rxd = pinctrl::get_pin(pinctrl::function::UARTE0_RXD);
+        CHECK(rxd >= 0);
+        CHECK((mem.get_value_at(uarte0_base + psel_rxd) & 0x1f) == rxd);
+
+        auto* txd_buffer = mem.get_ptr_at(uarte0_base + txd_ptr);
+        auto* rxd_buffer = mem.get_ptr_at(uarte0_base + rxd_ptr);
+
+        REQUIRE(txd_buffer != nullptr);
+        REQUIRE(rxd_buffer != nullptr);
+
+        REQUIRE(mem.get_value_at(uarte0_base + txd_maxcnt) != 0);
+        REQUIRE(mem.get_value_at(uarte0_base + rxd_maxcnt) != 0);
+
+        // Here we are just checking that we are not segfaulting
+        std::memset(txd_buffer, 0, mem.get_value_at(uarte0_base + txd_maxcnt));
+        std::memset(rxd_buffer, 0, mem.get_value_at(uarte0_base + rxd_maxcnt));
     }
 
     SECTION("UARTE1") {
