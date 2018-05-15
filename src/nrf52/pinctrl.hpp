24bdf9092e47b590f520c16d860a535c0b5de7d7
Maxim Sloyko <m.sloyko@gmail.com>
2018-05-14 19:57:29 -0700

    Changed pin configuration functions in nrf52, to make it easier to do generic configuration

--- src/nrf52/pinctrl.hpp
+++ src/nrf52/pinctrl.hpp
@@ -3,15 +3,21 @@
 namespace pinctrl {
 
 enum function {
+    UARTE_RTS = 1,
+    UARTE_TXD = 2,
+    UARTE_CTS = 3,
+    UARTE_RXD = 4,
     MIN_PIN_FUNCTION,
-    UARTE0_RXD,
-    UARTE0_CTS,
+    UARTE0_GROUP,
     UARTE0_RTS,
     UARTE0_TXD,
-    UARTE1_RXD,
-    UARTE1_CTS,
+    UARTE0_CTS,
+    UARTE0_RXD,
+    UARTE1_GROUP,
     UARTE1_RTS,
     UARTE1_TXD,
+    UARTE1_CTS,
+    UARTE1_RXD,
 
     MAX_PIN_FUNCTION,
 };
