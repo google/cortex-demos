3bb9217bea04516498453eb75cb0204a130048aa
Maxim Sloyko <m.sloyko@gmail.com>
2018-05-14 21:17:51 -0700

    cutils: Add ABSDIFF macro

--- src/cutils.h
+++ src/cutils.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #define ARRAY_SIZE(a)   (sizeof(a)/sizeof(a[0]))
+
+#define ABSDIFF(a, b) \
+    ({\
+        __typeof__(a) _a = (a);\
+        __typeof__(b) _b = (b);\
+        _a > _b ? (_a - _b) : (_b - _a);})
