f101cbba3dd3c86369ca74fd81574a8a9afcd3be
Maxim Sloyko <m.sloyko@gmail.com>
2018-05-22 20:40:33 -0700

    Add MIN and MAX functions to cutils

--- src/cutils.h
+++ src/cutils.h
@@ -7,3 +7,15 @@
         __typeof__(a) _a = (a);\
         __typeof__(b) _b = (b);\
         _a > _b ? (_a - _b) : (_b - _a);})
+
+#define MAX(a, b) \
+    ({\
+        __typeof__(a) _a = (a);\
+        __typeof__(b) _b = (b);\
+        _a > _b ? _a : _b;})
+
+#define MIN(a, b) \
+    ({\
+        __typeof__(a) _a = (a);\
+        __typeof__(b) _b = (b);\
+        _a < _b ? _a : _b;})
