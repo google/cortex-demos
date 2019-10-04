6805aadc82237312f7efc7944cfc945b88dc6fd3
Maxim Sloyko <maxims@google.com>
2019-10-04 16:11:35 -0700

    clk: Add some documentation for Generic Clock interface

--- src/clk.h
+++ src/clk.h
@@ -14,15 +14,60 @@
     limitations under the License.
 *******************************************************************************/
 
+/**
+ * @file
+ *
+ * Generic Clock interface.
+ *
+ * The interface defines generic functions for accessing and manipulating clocks.
+ */
+
 #pragma once
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief Request the clock to be initialized.
+ *
+ * @param clk_id Generic Clock Identifier. Implementations should define constants for specific clocks.
+ *
+ * @returns Zero on success, value less than zero (error code) on failure.
+ */
 int clk_request(int clk_id);
+
+/**
+ * @brief Set the option on the clock.
+ *
+ * This function is different from simple clk_request() in that it should not be used to start the clock.
+ * It can be used to, say, select the parent clock, enable/disable particular outputs etc.
+ * Avoid using this function for setting dividers. clk_request_rate() should be used for configuring
+ * the clock's rate.
+ *
+ * @param clk_id Generic Clock Identifier. Implementations should define constants for specific clocks.
+ * @param option Generic Option Identifier. Implementations should define constants for specific options.
+ *
+ * @returns Zero on success, value less than zero (error code) on failure.
+ */
 int clk_request_option(int clk_id, int option);
+
+/**
+ * @brief Get the clock's frequency (rate).
+ *
+ * @param clk_id Generic Clock Identifier. Implementations should define constants for specific clocks.
+ *
+ * @returns Clock frequency in Hz.
+ */
 unsigned int clk_get_rate(int clk_id);
+
+/**
+ * @brief Set the frequency (rate) of the clock.
+ *
+ * @param clk_id Generic Clock Identifier. Implementations should define constants for specific clocks.
+ *
+ * @returns The frequency that was actually set, which may be different from the one requested, if it's not supported.
+ */
 unsigned int clk_request_rate(int clk_id, unsigned int rate);
 
 #ifdef __cplusplus
