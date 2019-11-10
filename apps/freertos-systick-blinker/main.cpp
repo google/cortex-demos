5966ce9c8fa7142f28d5c382b63538d9ed1ec8de
Maxim Sloyko <maxims@google.com>
2019-11-10 09:31:02 -0800

    FreeRTOS: provide implementation of vApplicationGetIdleTaskMemory

--- apps/freertos-systick-blinker/main.cpp
+++ apps/freertos-systick-blinker/main.cpp
@@ -39,26 +39,6 @@ extern "C" {
     void xPortPendSVHandler(void);
     void vPortSVCHandler(void);
 
-    // TODO: Move to non-app specific location
-    void vApplicationGetIdleTaskMemory(StaticTask_t** ppxIdleTaskTCBBuffer,
-                                       StackType_t** ppxIdleTaskStackBuffer,
-                                       uint32_t* pulIdleTaskStackSize) {
-        static StaticTask_t xIdleTaskTCB;
-        static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-        /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
-        state will be stored. */
-        *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-        /* Pass out the array that will be used as the Idle task's stack. */
-        *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-        /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-        Note that, as the array is necessarily of type StackType_t,
-        configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-        *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-    }
-
 }  // extern "C"
 
 class BlinkerThread : public os::ThreadStatic<2 * configMINIMAL_STACK_SIZE> {
---
+++ third_party/FreeRTOS/Source/idle_task_memory.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+    Copyright 2019 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*******************************************************************************/
+
+#include "FreeRTOS.h"
+
+extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t** ppxIdleTaskTCBBuffer,
+                                              StackType_t** ppxIdleTaskStackBuffer,
+                                              uint32_t* pulIdleTaskStackSize) {
+    static StaticTask_t xIdleTaskTCB;
+#ifdef configIDLE_TASK_STACK_SIZE
+    static constexpr size_t stack_size = configIDLE_TASK_STACK_SIZE;
+#else
+    static constexpr size_t stack_size = configMINIMAL_STACK_SIZE;
+#endif
+    static StackType_t uxIdleTaskStack[stack_size];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
+       state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+       Note that, as the array is necessarily of type StackType_t,
+       configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = stack_size;
+}
